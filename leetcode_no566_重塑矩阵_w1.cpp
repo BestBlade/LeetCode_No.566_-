d043ddb349ba4f58b9ad6b6949f5b9bbb39a0f4a
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-17 19:58:34 +0800

    Add files via upload

---
+++ leetcode_no566_重塑矩阵_w1.cpp
@@ -0,0 +1,117 @@
+/* ------------------------------------------------------------------|
+在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+
+给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+
+重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+
+如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+
+示例 1:
+
+输入:
+nums =
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+输出:
+[[1,2,3,4]]
+解释:
+行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+示例 2:
+
+输入:
+nums =
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+输出:
+[[1,2],
+ [3,4]]
+解释:
+没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+注意：
+
+给定矩阵的宽和高范围在 [1, 100]。
+给定的 r 和 c 都是正数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reshape-the-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了99.55%的用户
+*	内存消耗：10.3 MB, 在所有 C++ 提交中击败了94.10%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+    int row = nums.size();
+    int col = nums[0].size();
+    if (row * col != r * c) {
+        return nums;
+    }
+
+    vector<vector<int>> res(r, vector<int>(c));
+    //for (int x = 0; x < r * c; x++) {
+    //    res[x / c][x % c] = nums[x / col][x % col];
+    //}
+    int p = 0;
+    int q = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (q == c) {
+                q = 0;
+                p++;
+            }
+            res[p][q++] = nums[i][j];
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<vector<int>> nums = { {1,2},{3,4} };
+    matrixReshape(nums, 1, 4);
+}
